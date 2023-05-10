090271526cdb3ad13674fff6f05c93fac72ddbf7
david <david.gama@ufms.br>
2023-05-10 16:32:28 -0400

    [REF] Ajustando rotas

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato 2')
-
-
-def sobre(request):
-    return HttpResponse('sobre 2')
+    return render(request, "recipes/home.html")
